f72f1bd2dc0cd6b29755c55f72fe892fcdb02d52
Brett g Porter <brett@bgporter.net>
2026-04-25 10:45:15 -0400

    Filter button in place but not connected.

--- src/view/headerView.cpp
+++ src/view/headerView.cpp
@@ -25,12 +25,44 @@
 #include "headerView.h"
 
 #include "palette.h"
+#include <ImageData.h>
+
+static juce::Image renderSvgWithColor (const void* data, int dataSize,
+                                        juce::Colour originalColor, juce::Colour newColor, int size)
+{
+    auto drawable = juce::Drawable::createFromImageData (data, dataSize);
+    drawable->replaceColour (originalColor, newColor);
+
+    juce::Image img (juce::Image::ARGB, size, size, true);
+    juce::Graphics g (img);
+    drawable->drawWithin (g, img.getBounds ().toFloat (),
+                          juce::RectanglePlacement::centred, 1.0f);
+    return img;
+}
 
 HeaderView::HeaderView (AppContext& context)
 : appContext { context }
 , persistentContext { appContext }
 , runtimeContext { appContext }
 {
+    Palette palette { persistentContext };
+    const juce::Colour iconColor = palette.defaultText.get ();
+    // 0xF3F3F3 is the fill color baked into all SVGs in assets/images
+    const auto svgFillColor = juce::Colour (0xFFF3F3F3);
+
+    const auto iconImage = renderSvgWithColor (
+        ImageData::filter_list_24dp_F3F3F3_FILL0_wght300_GRAD0_opsz24_svg,
+        ImageData::filter_list_24dp_F3F3F3_FILL0_wght300_GRAD0_opsz24_svgSize,
+        svgFillColor, iconColor, kHeight);
+
+    filterButton.setImages (
+        false, true, true,
+        iconImage, 0.7f, juce::Colours::transparentBlack,
+        iconImage, 1.0f, juce::Colours::transparentBlack,
+        iconImage, 1.0f, iconColor.withAlpha (0.2f));
+    filterButton.setClickingTogglesState (true);
+    filterButton.setButtonText ("filter");
+    addAndMakeVisible (filterButton);
 }
 
 void HeaderView::paint (juce::Graphics& g)
@@ -44,4 +76,8 @@ void HeaderView::paint (juce::Graphics& g)
 
 void HeaderView::resized ()
 {
+    auto bounds = getLocalBounds ();
+    const int buttonSize = kHeight - 8;
+    const int yOffset    = (getHeight () - buttonSize) / 2;
+    filterButton.setBounds (bounds.getRight () - buttonSize - 4, yOffset, buttonSize, buttonSize);
 }
--- src/view/headerView.h
+++ src/view/headerView.h
@@ -45,4 +45,5 @@ private:
     AppContext appContext;
     PersistentContext persistentContext;
     RuntimeContext runtimeContext;
+    juce::ImageButton filterButton;
 };
